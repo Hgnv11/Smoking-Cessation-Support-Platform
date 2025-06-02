9f30ec6eb80f29c5fb0ec032f189d9e14bac9c37
HaroldXL <kalkelvinxl@gmail.com>
2025-06-02 16:23:31 +0700

    add stuff to home screen (guest)

--- SmokingCessationSupportPlatform-FE/src/pages/home/home.jsx
+++ SmokingCessationSupportPlatform-FE/src/pages/home/home.jsx
@@ -1,7 +1,8 @@
 import Header from "../../components/header/header";
 import Poster from "../../components/poster/poster";
 import Footer from "../../components/footer/footer";
-import { Affix } from "antd";
+import "./home.css";
+import { Affix, Card } from "antd";
 
 function Home() {
   return (
@@ -10,7 +11,49 @@ function Home() {
         <Header />
       </Affix>
       <Poster />
-      <div>Home</div>
+      <div className="wrapper">
+        <div className="wrapper__title">
+          <p>Explore</p>
+        </div>
+        <div className="wrapper__card">
+          <Card hoverable className="wrapper__card-card">
+            <img
+              alt="example"
+              className="wrapper__card-img"
+              src="../src/components/images/card-asset1.png"
+            />
+            <h2 className="wrapper__card-title">I want to Quit</h2>
+            <p className="wrapper__card-des">
+              Quitting smoking is one of the best decisions for your health and
+              future
+            </p>
+          </Card>
+          <Card hoverable className="wrapper__card-card">
+            <img
+              alt="example"
+              className="wrapper__card-img"
+              src="../src/components/images/card-asset2.png"
+            />
+            <h2 className="wrapper__card-title">Community</h2>
+            <p className="wrapper__card-des">
+              Buy your medicines with our mobile application with a simple
+              delivery system
+            </p>
+          </Card>
+          <Card hoverable className="wrapper__card-card">
+            <img
+              alt="example"
+              className="wrapper__card-img"
+              src="../src/components/images/card-asset3.png"
+            />
+            <h2 className="wrapper__card-title">Consulation</h2>
+            <p className="wrapper__card-des">
+              Free consultation with our trusted doctors and get the best
+              recomendations
+            </p>
+          </Card>
+        </div>
+      </div>
       <Footer />
     </>
   );
