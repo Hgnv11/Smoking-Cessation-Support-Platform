19a8bb1e47b8a7b6506ca7d4be5edb4561c796e2
CrystalJohn <tranphanthanhphuc@gmail.com>
2025-06-13 20:31:45 +0700

    fix header error

--- SmokingCessationSupportPlatform-FE/src/components/header/header.jsx
+++ SmokingCessationSupportPlatform-FE/src/components/header/header.jsx
@@ -1,112 +1,52 @@
-import { Button, Avatar } from "antd";
+import { Button } from "antd";
 import "./header.css";
-import { SearchOutlined, UserOutlined } from "@ant-design/icons";
-import { useNavigate, useLocation } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { SearchOutlined } from "@ant-design/icons";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
   const navigate = useNavigate();
-  const location = useLocation();
 
-  const user = useSelector((store) => store.user);
-
-  const isActivePage = (path) => {
-    if (path === "/") {
-      return location.pathname === "/";
-    }
-    return location.pathname.includes(path);
-  };
-
-const Header = () => {
   return (
     <>
       <header className="header">
         <div className="header__container">
           <div className="header__inner">
-            <a href="/">
-              <img
-                className="header__inner-logo"
-                src="../src/components/images/Quitlt-logo.png"
-                alt="Quitlt Logo"
-              />
-            </a>
+            <img
+              className="header__inner-logo"
+              src="../src/components/images/Quitlt-logo.png"
+              alt="Quitlt Logo"
+            />
             <nav className="header__nav">
-              <a href="/" className={isActivePage("/") ? "active" : ""}>
-                Home
-              </a>
+              <a href="/">Home</a>
 
-              <a
-                href="/user-coach"
-                className={isActivePage("/user-coach") ? "active" : ""}
-              >
-                Coach
-              </a>
+              <a href="/#">Coach</a>
 
-              <a
-                href="/community"
-                className={isActivePage("/community") ? "active" : ""}
-              >
-                Community
-              </a>
-
-              <a href="/#" className={isActivePage("/#") ? "active" : ""}>
-                Article & Information
-              </a>
+              <a href="/#">Community</a>
+              <a href="/#">About Us</a>
             </nav>
           </div>
           <div className="header__login-register">
             <SearchOutlined className="search" />
-            {user ? (
-              // Hiển thị khi user đã đăng nhập
-              <div className="user-info">
-                {user.avatar ? (
-                  <Avatar
-                    onClick={() => navigate("/user-profile")}
-                    className="user-avatar"
-                    size="large"
-                    src={user.avatar}
-                  />
-                ) : (
-                  <Avatar
-                    onClick={() => navigate("/user-profile")}
-                    className="user-avatar"
-                    size="large"
-                    icon={<UserOutlined />}
-                  />
-                )}
-                <Button
-                  type="link"
-                  onClick={() => navigate("/user-profile")}
-                  className="user-name"
-                >
-                  {user.profileName}
-                </Button>
-              </div>
-            ) : (
-              // Hiển thị khi user chưa đăng nhập
-              <>
-                <Button
-                  type="primary"
-                  onClick={() => navigate("/login")}
-                  className="login-btn"
-                >
-                  Login
-                </Button>
-                <Button
-                  color="primary"
-                  variant="filled"
-                  onClick={() => navigate("/register")}
-                  className="register-btn"
-                >
-                  Sign Up
-                </Button>
-              </>
-            )}
+            <Button
+              type="primary"
+              onClick={() => navigate("/login")}
+              className="login-btn"
+            >
+              Login
+            </Button>
+            <Button
+              color="primary"
+              variant="filled"
+              onClick={() => navigate("/register")}
+              className="register-btn"
+            >
+              Sign Up
+            </Button>
           </div>
         </div>
       </header>
     </>
   );
-};
+}
 
 export default Header;
--- SmokingCessationSupportPlatform-FE/src/pages/home/home.jsx
+++ SmokingCessationSupportPlatform-FE/src/pages/home/home.jsx
@@ -3,35 +3,8 @@ import Poster from "../../components/poster/poster";
 import Footer from "../../components/footer/footer";
 import "./home.css";
 import { Affix, Card } from "antd";
-import { useNavigate } from "react-router-dom";
 
 function Home() {
-  const navigate = useNavigate();
-
-  const testimonials = [
-    {
-      id: 1,
-      text: "After 15 years of smoking, I never thought I'd quit. BreakFree's personalized plan and supportive community made it possible. I'm 8 months smoke-free now!",
-      avatar: "../src/components/images/avatar1.png",
-      name: "John Doe",
-      role: "Quit Smoker - 8 Months",
-    },
-    {
-      id: 2,
-      text: "After 15 years of smoking, I never thought I'd quit. BreakFree's personalized plan and supportive community made it possible. I'm 8 months smoke-free now!",
-      avatar: "../src/components/images/avatar2.png",
-      name: "Matthew Paul",
-      role: "Quit Smoker - 12 Months",
-    },
-    {
-      id: 3,
-      text: "After 15 years of smoking, I never thought I'd quit. BreakFree's personalized plan and supportive community made it possible. I'm 8 months smoke-free now!",
-      avatar: "../src/components/images/avatar3.png",
-      name: "Thien Phung",
-      role: "Quit Smoker - 6 Months",
-    },
-  ];
-
   return (
     <>
       <Affix offsetTop={0}>
@@ -43,11 +16,7 @@ function Home() {
           <p>Explore</p>
         </div>
         <div className="wrapper__card">
-          <Card
-            hoverable
-            className="wrapper__card-card"
-            onClick={() => navigate("/make-plan")}
-          >
+          <Card hoverable className="wrapper__card-card">
             <img
               alt="example"
               className="wrapper__card-img"
@@ -59,11 +28,7 @@ function Home() {
               future
             </p>
           </Card>
-          <Card
-            hoverable
-            className="wrapper__card-card"
-            onClick={() => navigate("/community")}
-          >
+          <Card hoverable className="wrapper__card-card">
             <img
               alt="example"
               className="wrapper__card-img"
@@ -87,35 +52,6 @@ function Home() {
               recomendations
             </p>
           </Card>
-        </div>
-        <div className="custom-divider"></div>
-
-        <div className="wrapper__title">
-          <p>Article & Information</p>
-        </div>
-
-        <div className="wrapper__title">
-          <p>What other users say about Quitlt</p>
-        </div>
-        <div className="wrapper__card">
-          {testimonials.map((testimonial) => (
-            <Card key={testimonial.id} hoverable className="wrapper__card-card">
-              <h3 className="wrapper__card-des">"{testimonial.text}"</h3>
-              <div className="wrapper__card-user">
-                <img
-                  alt="user"
-                  className="wrapper__card-user-img"
-                  src={testimonial.avatar}
-                />
-                <div className="wrapper__card-user-info">
-                  <h3 className="wrapper__card-user-name">
-                    {testimonial.name}
-                  </h3>
-                  <p className="wrapper__card-user-role">{testimonial.role}</p>
-                </div>
-              </div>
-            </Card>
-          ))}
         </div>
       </div>
       <Footer />
