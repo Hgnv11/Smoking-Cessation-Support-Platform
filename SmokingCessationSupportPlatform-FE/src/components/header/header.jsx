1e49a83d0ac8bb2b8d549569158507378ed2dbd4
HaroldXL <kalkelvinxl@gmail.com>
2025-06-14 15:38:35 +0700

    adjust UI

--- SmokingCessationSupportPlatform-FE/src/components/header/header.jsx
+++ SmokingCessationSupportPlatform-FE/src/components/header/header.jsx
@@ -1,47 +1,106 @@
-import { Button } from "antd";
+import { Button, Avatar } from "antd";
 import "./header.css";
-import { SearchOutlined } from "@ant-design/icons";
-import { useNavigate } from "react-router-dom";
+import { SearchOutlined, UserOutlined } from "@ant-design/icons";
+import { useNavigate, useLocation, Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function Header() {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const user = useSelector((store) => store.user);
+
+  const isActivePage = (path) => {
+    if (path === "/") {
+      return location.pathname === "/";
+    }
+    return location.pathname.includes(path);
+  };
 
   return (
     <>
       <header className="header">
         <div className="header__container">
           <div className="header__inner">
-            <img
-              className="header__inner-logo"
-              src="../src/components/images/Quitlt-logo.png"
-              alt="Quitlt Logo"
-            />
+            <a href="/">
+              <img
+                className="header__inner-logo"
+                src="../src/components/images/Quitlt-logo.png"
+                alt="Quitlt Logo"
+              />
+            </a>
             <nav className="header__nav">
-              <a href="/">Home</a>
+              <a href="/" className={isActivePage("/") ? "active" : ""}>
+                Home
+              </a>
 
-              <a href="/#">Coach</a>
+              <a
+                href="/user-coach"
+                className={isActivePage("/user-coach") ? "active" : ""}
+              >
+                Coach
+              </a>
 
-              <a href="/#">Community</a>
-              <a href="/#">About Us</a>
+              <a
+                href="/community"
+                className={isActivePage("/community") ? "active" : ""}
+              >
+                Community
+              </a>
+
+              <a href="/#" className={isActivePage("/#") ? "active" : ""}>
+                Article & Information
+              </a>
             </nav>
           </div>
           <div className="header__login-register">
             <SearchOutlined className="search" />
-            <Button
-              type="primary"
-              onClick={() => navigate("/login")}
-              className="login-btn"
-            >
-              Login
-            </Button>
-            <Button
-              color="primary"
-              variant="filled"
-              onClick={() => navigate("/register")}
-              className="register-btn"
-            >
-              Sign Up
-            </Button>
+            {user ? (
+              // Hiển thị khi user đã đăng nhập
+              <div className="user-info">
+                {user.avatar ? (
+                  <Avatar
+                    onClick={() => navigate("/user-profile")}
+                    className="user-avatar"
+                    size="large"
+                    src={user.avatar}
+                  />
+                ) : (
+                  <Avatar
+                    onClick={() => navigate("/user-profile")}
+                    className="user-avatar"
+                    size="large"
+                    icon={<UserOutlined />}
+                  />
+                )}
+                <Button
+                  type="link"
+                  onClick={() => navigate("/user-profile")}
+                  className="user-name"
+                >
+                  {user.profileName}
+                </Button>
+              </div>
+            ) : (
+              // Hiển thị khi user chưa đăng nhập
+              <>
+                <Button
+                  type="primary"
+                  onClick={() => navigate("/login")}
+                  className="login-btn"
+                >
+                  Login
+                </Button>
+                <Button
+                  color="primary"
+                  variant="filled"
+                  onClick={() => navigate("/register")}
+                  className="register-btn"
+                >
+                  Sign Up
+                </Button>
+              </>
+            )}
           </div>
         </div>
       </header>
